8ed05c4736935901b56c8015177134347c99b614
Samrridh17 <94668140+Samrridh17@users.noreply.github.com>
2023-07-20 15:15:03 +0530

    Update PostsWidget.jsx

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -9,7 +9,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   const token = useSelector((state) => state.token);
 
   const getPosts = async () => {
-    const response = await fetch("http://localhost:3000/posts", {
+    const response = await fetch("https://doodle-back.onrender.com/posts", {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
